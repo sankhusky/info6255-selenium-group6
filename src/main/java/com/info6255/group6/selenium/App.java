a34a90cb7fea07f27b188e113d85ae78bb2bbfcf
sankhusky <pimple.s@husky.neu.edu>
2020-11-22 12:40:54 -0500

    Adding an empty project

---
+++ src/main/java/com/info6255/group6/selenium/App.java
@@ -0,0 +1,13 @@
+package com.info6255.group6.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
