064a5a48eec04a51d611fbd57c1241d9f7f3507a
Vidhi Nagda <nagda.v@northeastern.edu>
2020-11-24 17:00:14 -0500

    Comments to Scenario 5

--- src/main/java/com/info6255/group6/selenium/testscenarios/Scenario5CoursePlan.java
+++ src/main/java/com/info6255/group6/selenium/testscenarios/Scenario5CoursePlan.java
@@ -11,7 +11,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-//TODO Add comments
+/**
+ * Create Plan for Course Registration
+ * 
+ */
 //TODO Call Utils.takeScreenShot(driver, "filename") wherever necessary for before + after actions
 public class Scenario5CoursePlan {
 //    @Test
@@ -23,9 +26,10 @@ public class Scenario5CoursePlan {
 		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Services & Links"))))
 				.click();
 		driver.findElement(By.linkText("Course Registration (NEW)")).click();
+		
 		String handle = driver.getWindowHandle();
 
-		// switch window to Banner
+		// switch to new window to open Banner
 		for (String handles : driver.getWindowHandles()) {
 			if (handles.equals(handle))
 				continue;
@@ -34,7 +38,7 @@ public class Scenario5CoursePlan {
 
 		driver.findElement(By.id("planningLink")).click();
 
-		// select term
+		// select term in order to create a plan
 		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("s2id_txt_term")))).click();
 		WebElement semSearch = driver.findElement(By.id("s2id_autogen1_search"));
 		semSearch.sendKeys("Spring 2021 Semester");
@@ -44,7 +48,7 @@ public class Scenario5CoursePlan {
 				.click();
 		driver.findElement(By.id("term-go")).click();
 		
-		// create plan
+		//Search courses and add them to create a plan
 		logger.log(Level.INFO, "Clicking create plan");
 		driver.findElement(By.id("createPlan")).click();
 		logger.log(Level.INFO, "adding course number");
@@ -53,6 +57,9 @@ public class Scenario5CoursePlan {
 		logger.log(Level.INFO, "searched course, waiting to load...");
 		// Thread.sleep(10000);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Using xpath to find the button Add Course to the Plan
+		
 		wait.until(ExpectedConditions
 				.elementToBeClickable(driver.findElement(By.xpath("//*[@id=\"table1\"]/tbody/tr/td[6]/div/button[2]"))))
 				.click();
@@ -66,16 +73,10 @@ public class Scenario5CoursePlan {
 		logger.log(Level.INFO, "input text for plan name");
 		logger.log(Level.INFO, "clicking save...");
 		driver.findElement(By.xpath("//div[@class=\"ui-dialog-buttonset\"]/button/span[contains(text(),'Save')]//parent::button")).click();
-//        Thread.sleep(2000);
+
 		logger.log(Level.INFO, "Saved the plan");
 		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		logger.log(Level.INFO, "plan display--");
 		
-//		Removing below step as it was extra and uncertain. - it was basically for displaying all plans
-//		driver.findElement(By.xpath("//*[@id=\"breadcrumbHeader\"]/a[@data-id=\"4\"]")).click();;
-//		wait.until(ExpectedConditions
-//				.elementToBeClickable()).click();
-//		logger.log(Level.INFO, "Plan was displayed");
-//		 driver.quit();
 	}
 }
